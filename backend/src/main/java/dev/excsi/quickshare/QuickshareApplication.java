62f459609e4928249cbd84dd130c1314ed450e38
Excsi <1aastreaa@gmail.com>
2026-08-21 16:11:27 -0400

    Initial commit

---
+++ backend/src/main/java/dev/excsi/quickshare/QuickshareApplication.java
@@ -0,0 +1,13 @@
+package dev.excsi.quickshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickshareApplication.class, args);
+	}
+
+}
